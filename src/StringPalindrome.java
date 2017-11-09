3fd58ce8e8f9e6fe238044ec57c1520ae5c1c357
Thomas George Thomas <tgt555@gmail.com>
2017-11-09 23:02:34 +0530

    First program

---
+++ src/StringPalindrome.java
@@ -0,0 +1,22 @@
+//WAP to check if the string is Palindrome
+import java.util.*;
+
+public class StringPalindrome {
+
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string:");
+		String s1=sc.nextLine();
+		StringBuilder s2=new StringBuilder(s1);
+		s2.reverse();
+	
+		if(s1.equals(s2.toString()))
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not a Palindrome");
+		
+		
+	}
+	
+}
